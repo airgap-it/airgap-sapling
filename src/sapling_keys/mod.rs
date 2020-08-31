eac2edd0d265c138601f93bcd285b0b584626a5a
Julia Samól <j.samol@papers.ch>
2020-08-31 08:47:09 +0200

    feat(viewing_key): generate viewing keys from seed

--- src/sapling_keys/mod.rs
+++ src/sapling_keys/mod.rs
@@ -1,7 +1,13 @@
 mod spending_key;
+mod viewing_key;
 mod derivation;
 
 pub use spending_key::{
     get_extended_spending_key,
     get_extended_spending_key_bytes,
+};
+
+pub use viewing_key::{
+    get_extended_full_viewing_key,
+    get_extended_full_viewing_key_bytes,
 };
