69d7433c7bdcd5b5e8397f332948f45d3ac9a925
Julia Samól <j.samol@papers.ch>
2020-08-31 15:35:45 +0200

    test(sapling-keys): improve tests, check if spending key is correct

--- src/sapling_keys/spending_key/extended_spending_key.rs
+++ src/sapling_keys/spending_key/extended_spending_key.rs
@@ -25,51 +25,73 @@ pub fn get_extended_spending_key_bytes(seed: &[u8], derivation_path: &str) -> Re
 mod test {
     use super::*;
     use crate::sapling_keys::derivation::DerivationPathError;
-    use bip39::{Language, Mnemonic, Seed};
+    use hex;
 
-    const MNEMONIC: &str = "sweet banner danger symptom ring skill benefit kitten appear much comic grab oak number coach";
-    const PASSWORD: &str = "";
-    const DERIVATION_PATH: &str = "m/";
+    const SEED: [u8; 32] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
+    // from https://github.com/zcash/zcash/blob/master/src/gtest/test_zip32.cpp
     #[test]
-    fn generates_extended_spending_key_from_mnemonic() {
-        let mnemonic = Mnemonic::from_phrase(MNEMONIC, Language::English).unwrap();
-        let seed = Seed::new(&mnemonic, PASSWORD);
+    fn generates_extended_spending_key_from_seed() {
+        let bytes = get_extended_spending_key_bytes(&SEED, "m/").unwrap();
+        let expected: Vec<u8> = [
+            "00", // depth (1 byte)
+            "00000000", // parent_fvk_tag (4 bytes)
+            "00000000", // child_index (4 bytes)
+            "d0947c4b03bf72a37ab44f72276d1cf3fdcd7ebf3e73348b7e550d752018668e", // chain_code (32 bytes)
+            "b6c00c93d36032b9a268e99e86a860776560bf0e83c1a10b51f607c954742506", // expsk.ask (32 bytes)
+            "8204ede83b2f1fbd84f9b45d7f996e2ebd0a030ad243b48ed39f748a8821ea06", // expsk.nsk (32 bytes)
+            "395884890323b9d4933c021db89bcf767df21977b2ff0683848321a4df4afb21", // expsk.ovk (32 bytes)
+            "77c17cb75b7796afb39f0f3e91c924607da56fa9a20e283509bc8a3ef996a172", // dk (32 bytes)
+        ].iter().flat_map(|&s| hex::decode(s).unwrap()).collect();
 
-        let _bytes = get_extended_spending_key_bytes(seed.as_bytes(), DERIVATION_PATH).unwrap();
-
-        // TODO: assert the key is correct
+        assert_eq!(bytes, expected);
     }
 
     #[test]
     fn fails_to_generate_extended_spending_key_when_derivation_path_empty() {
-        let mnemonic = Mnemonic::from_phrase(MNEMONIC, Language::English).unwrap();
-        let seed = Seed::new(&mnemonic, PASSWORD);
+        let error = get_extended_spending_key(&SEED, "").unwrap_err();
+        let error_bytes = get_extended_spending_key_bytes(&SEED, "").unwrap_err();
 
-        let error = get_extended_spending_key_bytes(seed.as_bytes(), "");
-
-        assert_eq!(error, Err(SpendingKeyError::caused_by(DerivationPathError::Empty.to_string())));
+        let expected = SpendingKeyError::caused_by(
+            DerivationPathError::Empty.to_string()
+        );
+        assert_eq!(error, expected);
+        assert_eq!(error_bytes, expected);
     }
 
     #[test]
     fn fails_to_generate_extended_spending_key_when_derivation_path_missing_prefix() {
-        let mnemonic = Mnemonic::from_phrase(MNEMONIC, Language::English).unwrap();
-        let seed = Seed::new(&mnemonic, PASSWORD);
+        let path = "/44'/123'/0'/0/0";
+        let error = get_extended_spending_key(&SEED, path).unwrap_err();
+        let error_bytes = get_extended_spending_key_bytes(&SEED, path).unwrap_err();
 
-        let error = get_extended_spending_key_bytes(seed.as_bytes(), "/44'/123'/0'/0/0");
-
-        assert_eq!(error, Err(SpendingKeyError::caused_by(DerivationPathError::MissingPrefix.to_string())));
+        let expected = SpendingKeyError::caused_by(
+            DerivationPathError::MissingPrefix.to_string()
+        );
+        assert_eq!(error, expected);
+        assert_eq!(error_bytes, expected);
     }
 
     #[test]
     fn fails_to_generate_extended_spending_key_when_derivation_path_invalid() {
-        let mnemonic = Mnemonic::from_phrase(MNEMONIC, Language::English).unwrap();
-        let seed = Seed::new(&mnemonic, PASSWORD);
+        let invalid_character_path = "m/44'/123q/0'/0/0";
+        let invalid_character = get_extended_spending_key(&SEED, invalid_character_path).unwrap_err();
+        let invalid_character_bytes = get_extended_spending_key_bytes(&SEED, invalid_character_path).unwrap_err();
 
-        let invalid_character = get_extended_spending_key_bytes(seed.as_bytes(), "m/44'/123q/0'/0/0");
-        let empty_index = get_extended_spending_key_bytes(seed.as_bytes(), "m/44'//0'/0/0");
+        let empty_index_path = "m/44'//0'/0/0";
+        let empty_index = get_extended_spending_key(&SEED, empty_index_path).unwrap_err();
+        let empty_index_bytes = get_extended_spending_key_bytes(&SEED, empty_index_path).unwrap_err();
 
-        assert_eq!(invalid_character, Err(SpendingKeyError::caused_by(DerivationPathError::invalid_character(vec!["q"]).to_string())));
-        assert_eq!(empty_index, Err(SpendingKeyError::caused_by(DerivationPathError::EmptyIndex.to_string())));
+        let invalid_character_expected = SpendingKeyError::caused_by(
+            DerivationPathError::invalid_character(vec!["q"]).to_string()
+        );
+        assert_eq!(invalid_character, invalid_character_expected);
+        assert_eq!(invalid_character_bytes, invalid_character_expected);
+
+        let empty_index_expected = SpendingKeyError::caused_by(
+            DerivationPathError::EmptyIndex.to_string()
+        );
+        assert_eq!(empty_index, empty_index_expected);
+        assert_eq!(empty_index_bytes, empty_index_expected);
     }
 }
--- src/sapling_keys/viewing_key/extended_full_viewing_key.rs
+++ src/sapling_keys/viewing_key/extended_full_viewing_key.rs
@@ -28,88 +28,71 @@ mod test {
         spending_key::SpendingKeyError,
         derivation::DerivationPathError,
     };
-    use bip39::{Language, Mnemonic, Seed};
 
-    const MNEMONIC: &str = "sweet banner danger symptom ring skill benefit kitten appear much comic grab oak number coach";
-    const PASSWORD: &str = "";
-    const DERIVATION_PATH: &str = "m/";
+    const SEED: [u8; 32] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
 
     #[test]
-    fn generates_extended_full_viewing_key_from_mnemonic() {
-        let mnemonic = Mnemonic::from_phrase(MNEMONIC, Language::English).unwrap();
-        let seed = Seed::new(&mnemonic, PASSWORD);
-
-        let _bytes = get_extended_full_viewing_key_bytes(seed.as_bytes(), DERIVATION_PATH).unwrap();
+    fn generates_extended_full_viewing_key_from_seed() {
+        let _bytes = get_extended_full_viewing_key_bytes(&SEED, "m/").unwrap();
 
         // TODO
     }
 
     #[test]
     fn fails_to_generate_extended_full_viewing_key_when_derivation_path_empty() {
-        let mnemonic = Mnemonic::from_phrase(MNEMONIC, Language::English).unwrap();
-        let seed = Seed::new(&mnemonic, PASSWORD);
+        let error = get_extended_full_viewing_key(&SEED, "").unwrap_err();
+        let error_bytes = get_extended_full_viewing_key_bytes(&SEED, "").unwrap_err();
 
-        let error = get_extended_full_viewing_key_bytes(seed.as_bytes(), "");
-
-        assert_eq!(
-            error,
-            Err(
-                ViewingKeyError::caused_by(
-                    SpendingKeyError::caused_by(
-                        DerivationPathError::Empty.to_string()
-                    ).to_string()
-                )
-            )
+        let expected = ViewingKeyError::caused_by(
+            SpendingKeyError::caused_by(
+                DerivationPathError::Empty.to_string()
+            ).to_string()
         );
+        assert_eq!(error, expected);
+        assert_eq!(error_bytes, expected);
     }
 
     #[test]
     fn fails_to_generate_extended_spending_key_when_derivation_path_missing_prefix() {
-        let mnemonic = Mnemonic::from_phrase(MNEMONIC, Language::English).unwrap();
-        let seed = Seed::new(&mnemonic, PASSWORD);
+        let path = "/44'/123'/0'/0/0";
+        let error = get_extended_full_viewing_key(&SEED, path).unwrap_err();
+        let error_bytes = get_extended_full_viewing_key_bytes(&SEED, path).unwrap_err();
 
-        let error = get_extended_full_viewing_key_bytes(seed.as_bytes(), "/44'/123'/0'/0/0");
-
-        assert_eq!(
-            error,
-            Err(
-                ViewingKeyError::caused_by(
-                    SpendingKeyError::caused_by(
-                        DerivationPathError::MissingPrefix.to_string()
-                    ).to_string()
-                )
-            )
+        let expected = ViewingKeyError::caused_by(
+            SpendingKeyError::caused_by(
+                DerivationPathError::MissingPrefix.to_string()
+            ).to_string()
         );
+
+        assert_eq!(error, expected);
+        assert_eq!(error_bytes, expected);
     }
 
     #[test]
     fn fails_to_generate_extended_spending_key_when_derivation_path_invalid() {
-        let mnemonic = Mnemonic::from_phrase(MNEMONIC, Language::English).unwrap();
-        let seed = Seed::new(&mnemonic, PASSWORD);
+        let invalid_character_path = "m/44'/123q/0'/0/0";
+        let invalid_character = get_extended_full_viewing_key(&SEED, invalid_character_path).unwrap_err();
+        let invalid_character_bytes = get_extended_full_viewing_key_bytes(&SEED, invalid_character_path).unwrap_err();
 
-        let invalid_character = get_extended_full_viewing_key_bytes(seed.as_bytes(), "m/44'/123q/0'/0/0");
-        let empty_index = get_extended_full_viewing_key_bytes(seed.as_bytes(), "m/44'//0'/0/0");
+        let empty_index_path = "m/44'//0'/0/0";
+        let empty_index = get_extended_full_viewing_key(&SEED, empty_index_path).unwrap_err();
+        let empty_index_bytes = get_extended_full_viewing_key_bytes(&SEED, empty_index_path).unwrap_err();
 
-        assert_eq!(
-            invalid_character,
-            Err(
-                ViewingKeyError::caused_by(
-                    SpendingKeyError::caused_by(
-                        DerivationPathError::invalid_character(vec!["q"]).to_string()
-                    ).to_string()
-                )
-            )
+        let invalid_character_expected = ViewingKeyError::caused_by(
+            SpendingKeyError::caused_by(
+                DerivationPathError::invalid_character(vec!["q"]).to_string()
+            ).to_string()
         );
+        assert_eq!(invalid_character, invalid_character_expected);
+        assert_eq!(invalid_character_bytes, invalid_character_expected);
 
-        assert_eq!(
-            empty_index,
-            Err(
-                ViewingKeyError::caused_by(
-                    SpendingKeyError::caused_by(
-                        DerivationPathError::EmptyIndex.to_string()
-                    ).to_string()
-                )
-            )
+        let empty_index_expected = ViewingKeyError::caused_by(
+            SpendingKeyError::caused_by(
+                DerivationPathError::EmptyIndex.to_string()
+            ).to_string()
         );
+        assert_eq!(empty_index, empty_index_expected);
+        assert_eq!(empty_index_bytes, empty_index_expected);
     }
 }
