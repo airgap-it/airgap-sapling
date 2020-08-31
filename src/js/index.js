61ead22f173ab5ed666aae79b053c3a509efac8a
Julia Samól <j.samol@papers.ch>
2020-08-31 08:53:48 +0200

    feat(viewing_key): add JS wrapper function

--- src/js/index.js
+++ src/js/index.js
@@ -16,11 +16,25 @@ export async function getExtendedSpendingKey(seed, derivationPath) {
   let seedBuffer
   if (Buffer.isBuffer(seed)) {
     seedBuffer = seed
-  } if (typeof seed === 'string') {
+  } else if (typeof seed === 'string') {
     seedBuffer = Buffer.from(seed, 'hex')
   } else {
     seedBuffer = Buffer.from(seed)
   }
 
   return Buffer.from(sapling.get_extended_spending_key(seedBuffer, derivationPath))
+}
+
+export async function getExtendedFullViewingKey(seed, derivationPath) {
+  const sapling = await saplingPromise
+  let seedBuffer
+  if (Buffer.isBuffer(seed)) {
+    seedBuffer = seed
+  } else if (typeof seed === 'string') {
+    seedBuffer = Buffer.from(seed, 'hex')
+  } else {
+    seedBuffer = Buffer.from(seed)
+  }
+
+  return Buffer.from(sapling.get_extended_full_viewing_key(seedBuffer, derivationPath))
 }
